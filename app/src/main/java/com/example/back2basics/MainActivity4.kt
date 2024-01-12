0dfc954269eff6b1d3604d70a673d46c3f9a2d73
Shelton17 <okangajeff17@gmail.com>
2024-01-12 12:10:47 +0300

    Add: Working with checkboxes and radio buttons

---
+++ app/src/main/java/com/example/back2basics/MainActivity4.kt
@@ -0,0 +1,30 @@
+package com.example.back2basics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import com.example.back2basics.databinding.ActivityMain4Binding
+
+class MainActivity4 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain4Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain4Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnOrder.setOnClickListener {
+            val checkVeggieRadioButtonId = binding.rgVeggies.checkedRadioButtonId
+            val veggie = findViewById<RadioButton>(checkVeggieRadioButtonId)
+            val cheese = binding.cbCheese.isChecked
+            val onions = binding.cbOnions.isChecked
+            val salad = binding.cbSalad.isChecked
+            val orderString = "You ordered a burger with: \n" +
+                    "${veggie.text}" +
+                    (if (cheese) "\nCheese" else "")+
+                    (if (onions) "\nOnions" else "")+
+                    (if (salad) "\nSalad" else "")
+            binding.tvOrder.text = orderString
+        }
+    }
+}
