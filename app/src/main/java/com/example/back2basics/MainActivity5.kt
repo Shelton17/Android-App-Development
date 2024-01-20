a8aa321f508612aafba3d534fd6680b754fe2183
Shelton17 <okangajeff17@gmail.com>
2024-01-20 19:47:24 +0300

    Update: Using spinners

--- app/src/main/java/com/example/back2basics/MainActivity5.kt
+++ app/src/main/java/com/example/back2basics/MainActivity5.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import com.example.back2basics.databinding.ActivityMain5Binding
 import com.example.back2basics.databinding.ActivityMainBinding
 
@@ -14,12 +17,31 @@ class MainActivity5 : AppCompatActivity() {
         binding = ActivityMain5Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        var country : String = ""
 
+        binding.spCountry.onItemSelectedListener = object  : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MainActivity5,
+                    "You're from ${parent?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_LONG).show()
+                country = parent?.getItemAtPosition(position).toString()
+            }
+        }
         binding.btnApply.setOnClickListener {
+
             val firstName = binding.etFirstName.text.toString()
             val lastName = binding.etLastName.text.toString()
             val birthDate = binding.etBirthDate.text.toString()
-            val country = binding.etCountry.text.toString()
+
             val person = Person(firstName,lastName,birthDate,country)
             Log.d("MainActivity", "$firstName $lastName, born on $birthDate, from $country just applied to the program")
             Intent(this, MainActivity6::class.java).also {
